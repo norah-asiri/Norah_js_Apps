5ce06e6d6436a3a608af014eab6b78b09f96b34f
norah-asiri <58185376+norah-asiri@users.noreply.github.com>
2021-09-15 05:20:01 +0300

    Add files via upload

---
+++ Asiri_Norah_JS_CreateVariables.js
@@ -0,0 +1,11 @@
+let RiderTall=0.0;  // This variable for save the rider tall. As float number
+let RiderAge=0; // This variable for save rider age as intger number
+const MiniTall= 52; // This variable to save the mini value of rider tall
+const MiniAge=10; // This variable to save the mini value of rider age
+
+//to check if Rider can ride the rollercoaster I use if statment:
+
+if (RiderTall>MiniTall && RiderAge>MiniAge)
+console.log("Get on that ride, kiddo!");
+else 
+console.log("Sorry kiddo. Maybe next year."); 
