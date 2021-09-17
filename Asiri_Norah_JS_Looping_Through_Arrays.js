d7a40dd4f940d8ab58ca78597d32689a642d9984
norah-asiri <58185376+norah-asiri@users.noreply.github.com>
2021-09-17 14:26:30 +0300

    Add files via upload

---
+++ Asiri_Norah_JS_Looping_Through_Arrays.js
@@ -0,0 +1,19 @@
+// #1
+var arr1 = [8, 6, 7, 5, 3, 0, 9];
+ for (var i=0 ; i<arr1.length; i++)
+ console.log(arr1[i]);
+
+ // #2
+var arr2 = [4, 7, 13, 13, 19, 37, -2];
+for (var i=0 ; i<arr2.length; i++)
+console.log(`sum ${i} + ${arr2[i]} = `+ (i+arr2[i]));
+
+
+// #3
+var arr3 = [6, 2, 12, 14, -24, 5, 0];
+for (var i=0 ; i<arr3.length; i++){
+ if (arr3[i]>5)
+    console.log(arr3[i])
+else 
+    arr3[i]= "No dice." 
+}
